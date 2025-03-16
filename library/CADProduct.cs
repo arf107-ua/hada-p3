b1f239348f3852cb8cec2e19c12c711539ac94d0
Julia Shifei Nieto Fernández <jsnf1@alu.ua.es>
2025-03-16 14:28:21 +0100

    70428580G Añadida library

---
+++ library/CADProduct.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+
+namespace library
+{
+    public class CADProduct
+    {
+        
+        //Inicializa la cadena de conexión de la BD.
+        public CADProduct()
+        {
+            string s = "data source=(LocalDBD)/MSSQLLocalDB;Integrated" +
+                "Security=SSPI;AttachDBFilename=|DataDirectory|\\Database.mdf";
+        }
+
+        //Crea un nuevo producto en la BD con
+        //los datos del producto representado por el parámetro en.
+        public bool Create(ENProduct en)
+        {
+            SqlConnection c = new SqlConnection(s);
+
+            try
+            {
+                c.Open();
+                SqlCommand com = new SqlCommand();
+            }
+        }
+
+        //Actualiza los datos de un producto en
+        //la BD con los datos del producto representado por el parámetro en.
+        public bool Update(ENProduct en)
+        {
+
+        }
+
+        //Borra el producto representado en en de la BD.
+        public bool Delete(ENProduct en)
+        {
+
+        }
+
+        //Devuelve solo el producto indicado leído de la BD. 
+        public bool Read(ENProduct en)
+        {
+
+        }
+
+        //Devuelve solo el primer producto de la BD
+        public bool ReadFirst(ENProduct en)
+        {
+
+        }
+
+        //Devuelve solo el producto siguiente al indicado.
+        public bool ReadNext(ENProduct en)
+        {
+
+        }
+
+        //Devuelve solo el producto anterior al indicado.
+        public bool ReadPrev(ENProduct en)
+        {
+
+        }
+    }
+}
---
+++ library/ENProduct.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace library
+{
+    internal class ENProduct
+    {
+        private string _code;
+        public string Code
+        {
+            get { return _code; }
+            set { _code = value; }
+        }
+
+        private string _name;
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        private int _amount;
+        public int Amount
+        {
+            get { return _amount; }
+            set { _amount = value; }
+        }
+
+        private float _price;
+        public float Price
+        {
+            get { return _price; }
+            set { _price = value; }
+        }
+
+        private int _category;
+        public int Category
+        {
+            get { return _category; }
+            set { _category = value; }
+        }
+
+        private DateTime _creationDate;
+        public DateTime CreationDate
+        {
+            get { return _creationDate; }
+            set { _creationDate = value; }
+        }
+
+        public ENProduct() { }
+       
+        public ENProduct(string code, string name, int amount, float price, int category, DateTime creationDate)
+        {
+            _code = code;
+            _name = name;
+            _amount = amount;
+            _price = price;
+            _category = category;
+            _creationDate = creationDate;
+        }
+
+        /*Guarda este producto en la BD.Para ello hará uso de
+        los métodos apropiados de CADProduct.Devuelve false si no se ha podido
+        realizar la operación.*/
+        public bool Create()
+        {
+            CADProduct cadProduct = new CADProduct();
+            return cadProduct.Create(this);
+        }
+
+        /*Actualiza este producto en la BD.Para ello hará uso
+        de los métodos apropiados de CADProduct.Devuelve false si no se ha
+        podido realizar la operación.*/
+        public bool Update()
+        {
+            CADProduct cadProduct = new CADProduct();
+            return cadProduct.Update(this);
+        }
+
+        /*Borra este producto de la BD.Para ello hará uso de
+        los métodos apropiados de CADProduct.Devuelve false si no se ha podido
+        realizar la operación.*/
+        public bool Delete()
+        {
+            CADProduct cadProduct = new CADProduct();
+            return cadProduct.Delete(this);
+        }
+
+        /*Recupera el producto indicado de la BD.Para ello hará
+        uso de los métodos apropiados de CADProduct.Devuelve false si no se ha
+        podido realizar la operación.*/
+        public bool Read()
+        {
+            CADProduct cadProduct = new CADProduct();
+            return cadProduct.Read(this);
+        }
+
+        /*Recupera todos los productos de la BD y devuelve
+        solo el primer producto.Para ello hará uso de los métodos apropiados de
+        CADProduct.Devuelve false si no se ha podido realizar la operación.*/
+        public bool ReadFirst()
+        {
+            CADProduct cadProduct = new CADProduct();
+            return cadProduct.ReadFirst(this);
+        }
+
+        /*Recupera todos los productos de la BD y devuelve
+         solo el producto siguiente al indicado. Para ello hará uso de los métodos
+        apropiados de CADProduct. Devuelve false si no se ha podido realizar la
+        operación.*/
+        public bool ReadNext()
+        {
+            CADProduct cadProduct = new CADProduct();
+            return cadProduct.ReadNext(this);
+        }
+
+        /*Recupera todos los productos de la BD y devuelve
+        solo el producto anterior al indicado. Para ello hará uso de los métodos
+        apropiados de CADProduct. Devuelve false si no se ha podido realizar la
+        operación.
+        */
+        public bool ReadPrev()
+        {
+            CADProduct cadProduct = new CADProduct();
+            return cadProduct.ReadPrev(this);
+        }
+    }
+}
